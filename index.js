8e90dfee826de858093f653fa902d6043e14975f
G55er <winstonevarist@outlook.com>
2023-05-09 09:03:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // declaring the varibales 
 const App = require('express')()
-const PORT = process.env.PORT || 4500 
+const PORT = process.env.PORT || 4500
 const dB = 'mocoservices'
-const hostname = '0.0.0.0'
-const uri = `mongodb://${hostname}/${PORT}`
+const hostname = '127.0.0.1'
+// const uri = `mongodb://${hostname}/27017`
 
 // allow the form data 
 App.use(require('express')())
@@ -20,16 +20,19 @@ App.use(require('helmet')())
 // starting the development server and connecting to the database 
 const server = async function () {
     try {
-        const connected = await require('mongoose').connect(`${uri}`)
-        if (connected) {
-            App.listen(PORT, function () {
-                console.log(`Server is running at http://${hostname}:${PORT} and database is connected to the ${dB}`)
-            })
-            App.get('/', async function (req, res) {
-                res.send(`Server is running at http://${hostname}:${PORT}`)
-            })
-        }
-        else console.log(`Failed to connect to the ${dB}`)
+        // const connected = await require('mongoose').connect(`${uri}`)
+        // if (connected) {
+        // App.listen(PORT, function () {
+        //     console.log(`Server is running at http://${hostname}:${PORT} and database is connected to the ${dB}`)
+        // })
+        // App.get('/', async function (req, res) {
+        //     res.send(`Server is running at http://${hostname}:${PORT}`)
+        // })
+        App.listen(PORT, function () {
+            console.log(`Server is runing at http://${hostname}:${PORT}`)
+        })
+        // }
+        // else console.log(`Failed to connect to the ${dB}`)
     }
     catch (error) { console.log(error.message) }
 }
